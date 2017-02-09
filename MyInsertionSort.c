856cd39fadc00dd817c847c0ba6ba597a1bf953e
edcarter <edcarter@ualberta.ca>
2017-02-09 14:41:55 -0700

    remove copeid parameter

--- MyInsertionSort.c
+++ MyInsertionSort.c
@@ -9,19 +9,16 @@ JNIEXPORT jintArray JNICALL Java_MyInsertionSort_insertionsort
 	jsize len;
 	jint *my_array;
 	jint *result;
-	jboolean *is_copy = 0;
 
 	len = (*env)->GetArrayLength(env, buf);
-	my_array = (jint *) (*env)->GetIntArrayElements(env, buf, 0/*is_copy*/);
+	my_array = (jint *) (*env)->GetIntArrayElements(env, buf, 0);
 	if (my_array == NULL) {
 		printf("Cannot obtain array from JVM\n");
 		exit(0);
 	}
 	for (int i = 0; i < len; i++) my_array[i] = len - i - 1;
 	
-	//if (*is_copy == JNI_TRUE) {
-		(*env)->ReleaseIntArrayElements(env, buf, my_array, (jint)0); 
-	//}
+	(*env)->ReleaseIntArrayElements(env, buf, my_array, 0); 
 	return buf;
 }
 
