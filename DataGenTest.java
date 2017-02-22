916d183481624289361a79c267c9bc25df2fb492
edcarter <edcarter@ualberta.ca>
2017-02-21 20:19:23 -0700

    added datagen test

---
+++ DataGenTest.java
@@ -0,0 +1,27 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+
+/**
+ * Created by elias on 21/02/17.
+ */
+public class DataGenTest {
+    @Test
+    public void main() throws Exception {
+        int size = 1000;
+        String testPath = "deleteme";
+        DataGen.main(new String[] {Integer.toString(size), testPath});
+
+        int count = 0;
+        try (BufferedReader br = new BufferedReader(new FileReader(testPath))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                Integer.parseInt(line); //try to parse every line to make sure it is formatted correctly
+                count++;
+            }
+        }
+        Assert.assertEquals(size, count);
+    }
+}
